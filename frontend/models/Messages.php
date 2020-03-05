33c2ed1e751c690b32f2d1bd74d8ed5bddc61da9
Reymezis <qq1s@bk.ru>
2020-03-05 13:24:29 +0600

    create ActiveRecord Models via gii

---
+++ frontend/models/Messages.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "messages".
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property string|null $message
+ *
+ * @property Users $user
+ */
+class Messages extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'messages';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id'], 'integer'],
+            [['message'], 'string'],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'message' => 'Message',
+        ];
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery|UsersQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(Users::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return MessagesQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new MessagesQuery(get_called_class());
+    }
+}
