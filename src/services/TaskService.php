8b0b959bc839ce7de88f3df104ae1c103bfea5e1
Reymezis <qq1s@bk.ru>
2020-04-27 16:33:14 +0600

    add upload folder exist check

--- src/services/TaskService.php
+++ src/services/TaskService.php
@@ -26,9 +26,13 @@ class TaskService
         $task->save();
         $idTask = $task->id;
         $form->files = MyUploadedFile::getInstances($form, 'files');
+        $uploadsDir = __DIR__ . '/../../frontend/web/uploads/';
+        if (!is_dir($uploadsDir) && !mkdir($uploadsDir) && !is_dir($uploadsDir)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $uploadsDir));
+        }
         foreach ($form->files as $file) {
             /** Сохранение загруженных файлов в папку проекта uploads */
-            $file->saveAs('uploads/' . $file->getName());
+            $file->saveAs($uploadsDir . $file->getName());
 
             /** Сохранение имени файла и id задания в БД */
             $taskFiles = new Attachments();
